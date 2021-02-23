a045bcf9022a759d441b67b950e10e41a776542e
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-02-23 16:29:11 -0300

    add profile files

---
+++ moveit-next/src/components/Profile.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+    return (
+        <div className={ styles.profileContainer }>
+            <img src="https://avatars.githubusercontent.com/u/79108633?s=460&u=0b9b32d8c1b82b3f35f34812f2ce53358aae34fb&v=4" alt="Foto do usuario" />
+            <div>
+                <strong>Kayque Gonçalvaves</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    )
+}
